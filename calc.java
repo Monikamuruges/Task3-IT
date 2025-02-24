04a3f370722768807c1d8c06bd3ff1fc1fd06c9c
SurendhiranRajendhran <117349526+SurendhiranRajendhran@users.noreply.github.com>
2025-02-24 19:39:57 +0530

    commit by feature2

--- calc.java
+++ calc.java
@@ -5,6 +5,17 @@ public class Calc {
     public static int subtract(int a, int b) {
         return a - b;
     }
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static int div(int a, int b) {
+       if (b == 0) {
+          throw new ArithmeticException("Division by zero is not allowed");
+    }
+        return a / b;
+}
+
     public static void main(String[] args) {
         System.out.println("Calculator program");
     }

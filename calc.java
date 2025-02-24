068f59fdc14abb5b6432997cdeb8526242c78f92
Nithiyadharshini G <126751134+Nithiya-22it38@users.noreply.github.com>
2025-02-24 16:38:02 +0530

    Added calc.java - feature1

--- calc.java
+++ calc.java
@@ -1,1 +1,11 @@
-
+public class Calc {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Calculator program");
+    }
+}
